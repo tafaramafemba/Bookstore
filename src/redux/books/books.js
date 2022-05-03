c488c09e9e265bf65728c590d757568473a9025e
tafaramafemba <tafara.maf@gmail.com>
2022-05-03 22:18:51 +0200

    implement display book and add book functionality

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,46 +1,34 @@
+import { v4 as uuidv4 } from 'uuid';
+
 const ADD_BOOK = 'BOOK_ADDED';
 const REMOVE_BOOK = 'BOOK_REMOVED';
 
-export const moreBooks = () => {
+export function moreBooks(book){
+  const id = uuidv4();
   return {
     type: ADD_BOOK,
-    book
+    data: { ...book, id },
   }
 }
 
-export const lessBooks = () => {
+export function lessBooks(id){
   return {
-    type: REMOVE_BOOK
+    type: REMOVE_BOOK,
+    id,
   }
 }
 
-const bookReducer = (state = {
-  books: [
-    {
-      id: 1,
-      title: "The Brotherhood",
-      author: "Tafara Mafemba"
-    },
-    {
-      id: 2,
-      title: "Raising Dion",
-      author: "Tom Ford"
-    },
-    {
-      id: 3,
-      title: "The Master Builder",
-      author: "Harry Styles"
-    },
-  ]
+const initialState = [
+  { id: uuidv4(), title: 'Title', author: 'Tafara' },
+  { id: uuidv4(), title: 'Chronicles of Tino', author: 'Tino'}
+]
 
-}, action) => {
+export default function (state = initialState, action) {
   switch (action.type) {
-    case ADD_BOOK: return [...state, action.book];
+    case ADD_BOOK: return [...state, action.data]
 
-    case REMOVE_BOOK: return [...state]
+    case REMOVE_BOOK: return state.filter((book) => book.id !== action.id);
     default: return state
   }
 
 }
-
-export default bookReducer;
